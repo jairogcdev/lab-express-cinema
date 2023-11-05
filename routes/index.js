a64e886f3667bc0940f35bb9f2f1f2cf811945cd
jairohrdev <j.gcarballa.dev@gmail.com>
2023-11-05 22:58:52 +0100

    Lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movies = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res, next) => {
+  try {
+    let movies = await Movies.find();
+    res.render("movies", { movies });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/movie/:id", async (req, res, next) => {
+  try {
+    let movie = await Movies.findById(req.params.id);
+    console.log("_----------------------------------------------------_");
+    console.log(movie);
+    res.render("details", { movie });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
